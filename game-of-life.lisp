ff422a08cd64ef57254d24ed205698717a5e1f94
Clayton Allen <mrchafeits@aaathats3as.com>
2020-01-19 17:09:07 -0800

    feat: adding gui skeleton

--- game-of-life.lisp
+++ game-of-life.lisp
@@ -1,3 +1,30 @@
+;;; game-of-life.lisp --- Game of life using lispbuilder-sdl
+
+; (setq *load-verbose* nil)
+
+(ql:quickload 'lispbuilder-sdl)
+;; (ql:quickload 'lispbuilder-sdl-gfx)
+(ql:quickload 'unix-opts)
+
+(defparameter *width* 200)
+(defparameter *height* 200)
+
+(defparameter *config-view-delta* 2)
+(defparameter *config-timestep* 1.0)
+(defparameter *config-timestep-delta* 2)
+
+(defparameter *config-window-x* 512)
+(defparameter *config-window-y* 512)
+
+(defun update-view-size ()
+  (setf *config-view-x* (round (/ *config-window-x* *config-view-size*)))
+  (setf *config-view-y* (round (/ *config-window-y* *config-view-size*))))
+
+(defparameter *config-view-size* 16)
+(defvar *config-view-x*)
+(defvar *config-view-y*)
+(update-view-size)
+
 ;; (let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
 ;;                                        (user-homedir-pathname))))
 ;;   (when (probe-file quicklisp-init)
@@ -95,6 +122,94 @@
 (defparameter height nil)
 (defparameter generation nil)
 
+(defclass game ()
+  ((paused :accessor game-paused :initform t)
+   ;; current gen of life
+   (state :accessor game-state :initform nil)
+   ;; current view offset
+   (vx :accessor game-vx :initform 0)
+   (vy :accessor game-vy :initform 0)
+   ;; SDL window
+   (window :accessor game-window)))
+
+(defmethod game-toggle-paused ((g game))
+  (setf (game-paused g) (not (game-paused g))))
+
+(defmethod game-step ((g game))
+  (format "TODO~%"))
+
+(defmethod game-reset ((g game))
+  (format "TODO~%"))
+
+(defmethod game-run ((g game))
+  "Game-Of-Life docstring."
+  (sdl:with-init ()
+    ;; set up key repeat so holding down a key works
+    (sdl:enable-key-repeat 200 30)
+    (sdl:window width height :title-caption "Common Lisp Life")
+    (setf (sdl:frame-rate) 60)
+    (sdl:clear-display (sdl:color :r 127 :g 127 :b 127))
+
+    (loop for i from 0 to height by 20
+          do (progn (sdl:draw-box (sdl:rectangle :x 0 :y i :w width :h 10)
+                                  :color (sdl:color))
+                    (sdl:draw-box (sdl:rectangle :x i :y 0 :w 10 :h height)
+                                  :color (sdl:color :r 255 :g 255 :b 255))))
+    (sdl:update-display)
+    (sdl:with-events (:poll)
+      (:quit-event () t)
+      (:video-expose-event () (sdl:update-display))
+      ;; process keyboard input
+      ;; make living cell with [MouseLeftClick]
+      (:key-down-event (:key key)
+       ;; close window with [RedX] or [ESC]
+       (when (sdl:key= key :sdl-key-escape)
+         (sdl:push-quit-event))
+       ;; move view area with [W A S D] or [MouseDrag]
+       (when (or (sdl:key= key :sdl-key-a) (sdl:key= key :sdl-key-left))
+         (incf (game-vx g) *config-view-delta*))
+       (when (or (sdl:key= key :sdl-key-d) (sdl:key= key :sdl-key-right))
+         (decf (game-vx g) *config-view-delta*))
+       (when (or (sdl:key= key :sdl-key-w) (sdl:key= key :sdl-key-up))
+         (decf (game-vy g) *config-view-delta*))
+       (when (or (sdl:key= key :sdl-key-s) (sdl:key= key :sdl-key-down))
+         (incf (game-vy g) *config-view-delta*))
+       ;; zoom view area with [+ -] or [MouseWheel]
+       (when (sdl:key= key :sdl-key-minus)
+         (when (> *config-view-size* 2) ;; minimum of 2x2
+           (decf *config-view-size* 1)
+           (update-view-size)))
+       (when (sdl:key= key :sdl-key-plus)
+         (when (< *config-view-size* (+ *config-window-x* 1)) ;; TODO: square window only?
+           (incf *config-view-size* 1)
+           (update-view-size)))
+       (when (sdl:key= key :sdl-key-equals)
+         (setf *config-view-size* 16)
+         (update-view-size))
+       ;; reset game state and pause with [R]
+       (when (sdl:key= key :sdl-key-r)
+         (game-reset g)
+         (setf (game-paused g) t))
+       ;; toggle pause sim with [P]
+       (when (sdl:key= key :sdl-key-p)
+         (game-toggle-paused g))
+       ;; adjust timestep with [< >] or [MouseWheel+SHIFT]
+       (when (sdl:key= key :sdl-key-less)
+         (when (< *config-timestep* most-positive-single-float)
+           (setf *config-timestep* (* *config-timestep* *config-timestep-delta*))))
+       (when (sdl:key= key :sdl-key-greater)
+         (when (> *config-timestep* 0.0)
+           (setf *config-timestep* (/ *config-timestep* *config-timestep-delta*))))
+       ; TODO mouse input
+       )
+      ;; if no input
+      (:idle ()
+       ;; todo
+       ;; update game at end
+       (game-step g)
+       (sdl:update-display))
+      )))
+
 (defun main ()
   (if (/= (length *posix-argv*) 3)
 	  (print "usage: sbcl --script game-of-life.lisp width height")
@@ -120,8 +235,8 @@
 
 			  (graphics-init generation))))))
 
-(main)
-
+(sb-int:with-float-traps-masked (:invalid :inexact :overflow)
+                                (main))
 ;; (setf (aref generation 4) 1)
 ;; (print generation)
 ;; (print (count-neighbors generation 5 0 width height))
